0d33871dbf92c6037808a798ab6cc4c6098dae80
Ilan Schnell <ilanschnell@gmail.com>
2021-07-11 02:25:41 -0500

    make wat code more readable

--- wat/collatz.py
+++ wat/collatz.py
@@ -1,40 +1,40 @@
 from wasmtime import Store, Module, Instance
 
 
-N = 100_000_000
+N = 10_000
 
 store = Store()
 module = Module(store.engine, """
 (module
-    (func $collatz (param i64) (result i64)
+    (func $collatz (param $n i64) (result i64)
        (local i64)
        i64.const 0
        local.set 1                    ;; j = 0   (result)
        (block
            (loop                      ;; label B
-               local.get 0            ;; n
+               local.get $n           ;; n
                i64.const 1
                i64.eq
                br_if 1                ;; if n == 1: goto A
                (block
                    (block
-                       local.get 0    ;; n
+                       local.get $n
                        i64.const 2
                        i64.rem_u      ;; n % 2
                        i32.wrap_i64
                        br_if 0        ;; if n % 2: goto D
-                       local.get 0    ;; n
+                       local.get $n
                        i64.const 2
                        i64.div_u
-                       local.set 0    ;; n = n / 2
+                       local.set $n   ;; n = n / 2
                        br 1           ;; goto C
                    )                  ;; label D
                    i64.const 3
-                   local.get 0        ;; n
+                   local.get $n
                    i64.mul
                    i64.const 1
                    i64.add
-                   local.set 0        ;; n = 3 * n + 1
+                   local.set $n       ;; n = 3 * n + 1
                )                      ;; label C
                local.get 1
                i64.const 1
