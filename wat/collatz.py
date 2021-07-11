0ff73346c5931d99db8dad3499e722fcaf678254
Ilan Schnell <ilanschnell@gmail.com>
2021-07-11 02:35:57 -0500

    update work

--- wat/collatz.py
+++ wat/collatz.py
@@ -4,50 +4,7 @@ from wasmtime import Store, Module, Instance
 N = 10_000
 
 store = Store()
-module = Module(store.engine, """
-(module
-    (func $collatz (param $n i64) (result i64)
-       (local i64)
-       i64.const 0
-       local.set 1                    ;; j = 0   (result)
-       (block
-           (loop                      ;; label B
-               local.get $n           ;; n
-               i64.const 1
-               i64.eq
-               br_if 1                ;; if n == 1: goto A
-               (block
-                   (block
-                       local.get $n
-                       i64.const 2
-                       i64.rem_u      ;; n % 2
-                       i32.wrap_i64
-                       br_if 0        ;; if n % 2: goto D
-                       local.get $n
-                       i64.const 2
-                       i64.div_u
-                       local.set $n   ;; n = n / 2
-                       br 1           ;; goto C
-                   )                  ;; label D
-                   i64.const 3
-                   local.get $n
-                   i64.mul
-                   i64.const 1
-                   i64.add
-                   local.set $n       ;; n = 3 * n + 1
-               )                      ;; label C
-               local.get 1
-               i64.const 1
-               i64.add
-               local.set 1            ;; j = j + 1
-               br 0                   ;; goto B     (continue)
-           )
-       )                              ;; label A
-       local.get 1
-    )
-    (export "collatz" (func $collatz))
-)
-""")
+module = Module.from_file(store.engine, './collatz.wat')
 
 instance = Instance(store, module, [])
 
