315625739afe5b20d49fc76c7af62b6ac450228a
Ilan Schnell <ilanschnell@gmail.com>
2021-07-14 23:46:25 -0500

    simplify main.py

--- main.py
+++ main.py
@@ -8,13 +8,9 @@ wat_file = 'u.wat'
 
 t_file(py_file, wat_file, debug=0)
 
-import demo
 import loader
 import u
 
-for i in range(0, 100):
-    assert demo.foo(i) == u.foo(i)
-
 t0 = time()
-print(u.foo(1000_000_000))
-print('%.3f sec' % (time() - t0))
+result = u.foo(1000_000_000)
+print('%d    %.3f sec' % (result, time() - t0))
